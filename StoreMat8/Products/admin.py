396b47b052948b3a4d5aeca0b9f84cedc3188d7d
MatinMat8 <matinmat8.mat@gmail.com>
2021-08-26 17:05:43 +0430

    set up the admin product model class

--- StoreMat8/Products/admin.py
+++ StoreMat8/Products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'brand', 'price')
+    search_fields = ('title', 'description', 'category', 'brand', 'slug')
+    list_filter = ('brand', 'category')
+    prepopulated_fields = {'slug': ('title', 'brand', 'category')}
