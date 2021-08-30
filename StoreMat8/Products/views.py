c50b3cf1bf549dc8be1439d95855ff8b6348a72e
MatinMat8 <matinmat8.mat@gmail.com>
2021-08-30 17:15:19 +0430

    Added the index view for display the products

--- StoreMat8/Products/views.py
+++ StoreMat8/Products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Product
 
-# Create your views here.
+
+# Show all the products
+class index(View):
+
+    def get(self, request):
+        products = Product.objects.all()
+        return render(request, 'Products/index.html', context={'products': products})
