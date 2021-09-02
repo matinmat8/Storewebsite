1e85ce724efac342d3e5e0d5b202981d7722c7a7
MatinMat8 <matinmat8.mat@gmail.com>
2021-09-02 17:35:44 +0430

    Added "add_to_cart" view for add a product to the cart

--- StoreMat8/Products/urls.py
+++ StoreMat8/Products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import *
 
 app_name = 'Products'
 
 urlpatterns = [
     path('', index.as_view(), name='index'),
+    path('add-to-cart/<slug:slug>/', add_to_cart, name='add-to-cart'),
 ]
--- StoreMat8/Products/views.py
+++ StoreMat8/Products/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.core.checks import messages
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from django.views import View
-from .models import Product
+from .models import Product, OrderItem, Order
 
 
 # Show all the products
@@ -9,3 +11,34 @@ class index(View):
     def get(self, request):
         products = Product.objects.all()
         return render(request, 'Products/index.html', context={'products': products})
+
+
+def add_to_cart(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    order_item, created = OrderItem.objects.get_or_create(
+        item=product,
+        user=request.user,
+        ordered=False
+    )
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # Check if the order item in in order
+        if order.items.filter(item__slug=product.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+            messages.Info(request, 'This item quantity was updated.')
+            return redirect("Products:index")
+        else:
+            order.items.add(order_item)
+            messages.Info(request, "this item was added to your cart")
+            return redirect("Products:index")
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(
+            user=request.user, ordered_date=ordered_date
+        )
+        order.items.add(order_item)
+        messages.Info(request, "This item was added to your cart.")
+        return redirect("Products:index", slug=slug)
+
