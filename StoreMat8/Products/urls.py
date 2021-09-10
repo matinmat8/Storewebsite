c61f787c68a90048f4043272d5622133dfe8e000
MatinMat8 <matinmat8.mat@gmail.com>
2021-09-10 13:30:42 +0430

    added a path for order summery view

--- StoreMat8/Products/urls.py
+++ StoreMat8/Products/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('detail/<slug:slug>/', DetailProduct.as_view(), name='detail'),
     path('add-to-cart/<slug:slug>/', add_to_cart, name='add-to-cart'),
     path('remove-an-item/<slug:slug>/', remove_an_item_from_cart, name='remove-an-item'),
+    path('order-summery/', OrderSummaryView.as_view(), name='order_summery'),
 ]
