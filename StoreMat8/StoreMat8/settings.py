2a9fcc0efc4a200d3ac426b95542820ce9fb80c7
MatinMat8 <matinmat8.mat@gmail.com>
2021-10-01 01:48:02 +0330

    added a path for register login app

--- StoreMat8/StoreMat8/settings.py
+++ StoreMat8/StoreMat8/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'register_login',
 
     # My apps
     'Products',
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = 'Products:index'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- StoreMat8/StoreMat8/urls.py
+++ StoreMat8/StoreMat8/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Products.urls', namespace='Products')),
+    path('authentication/', include('register_login.urls', namespace='register_login')),
 ]
