bc8672aea3236c10a13970cbbd913307ccb5fc38
MatinMat8 <matinmat8.mat@gmail.com>
2021-10-01 01:47:25 +0330

    added register_login app for authentication

---
+++ StoreMat8/register_login/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls import url
+from django.urls import path
+from .views import *
+from django.contrib.auth import views as auth_views
+from Products.views import ProductList
+
+app_name = 'register_login'
+
+urlpatterns = [
+    # path('', ProductList.as_view(), name='ProductList'),
+    path('register/', register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/Login.html', redirect_field_name='index')),
+
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(template_name="registration/reset_password.html"),
+         name="reset_password"),
+
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"),
+         name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
+
+    path('reset_password_completed/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_completed"),
+]
