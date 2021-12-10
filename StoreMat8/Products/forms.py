597c4f84e8e70f1dac6dba2fce06e61b74de97e0
MatinMat8 <matinmat8.mat@gmail.com>
2021-12-11 00:48:05 +0330

    change form to ModelForm

--- StoreMat8/Products/forms.py
+++ StoreMat8/Products/forms.py
@@ -1,9 +1,12 @@
 from django import forms
+from Products.models import DiscountSystem
 
 
 class SearchForm(forms.Form):
     search = forms.CharField()
 
 
-class DiscountCode(forms.Form):
-    discount_code = forms.CharField()
+class DiscountCode(forms.ModelForm):
+    class Meta:
+        model = DiscountSystem
+        fields = ['discount_code']
