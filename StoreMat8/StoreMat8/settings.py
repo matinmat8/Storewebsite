67534fc70c5c94b0c5359b957d80a1ac54743971
MatinMat8 <matinmat8.mat@gmail.com>
2022-02-23 23:43:41 +0330

    adding Login Url path in settings.py

--- StoreMat8/StoreMat8/settings.py
+++ StoreMat8/StoreMat8/settings.py
@@ -128,9 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = 'Products:index'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'Products:index'
+LOGIN_URL = 'register_login:login'
