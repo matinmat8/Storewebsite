4d945cb1aff7abe6dc0108a7afc2b4ecce9d17b0
MatinMat8 <matinmat8.mat@gmail.com>
2022-03-19 02:40:14 +0330

    Adjusting the detail url

--- StoreMat8/Products/urls.py
+++ StoreMat8/Products/urls.py
@@ -5,9 +5,10 @@ app_name = 'Products'
 
 urlpatterns = [
     path('', CategoryPage.as_view(), name='index'),
+    path('following_categories/<int:pk>', FollowingCategories.as_view(), name='FollowingCategories'),
     path('products/', ProductList.as_view(), name='products'),
     path('search_view/', SearchProduct.as_view(), name='search'),
-    path('detail/<slug:slug>/<category>/', DetailProduct.as_view(), name='detail'),
+    path('detail/<slug:slug>/<int:pk>/', DetailProduct.as_view(), name='detail'),
     path('add-to-cart/<slug:slug>/', AddToCart.as_view(), name='add-to-cart'),
     path('remove-from-cart/<slug:slug>/', RemoveFromCart.as_view(), name='remove-from-cart'),
     path('remove-an-item/<slug:slug>/', RemoveAnItemFromCart.as_view(), name='remove-an-item'),
